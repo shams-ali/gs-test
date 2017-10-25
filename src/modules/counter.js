30393ba28cb8c3ad9d32645b17f5b986a5ad522a
Shams Ali <ali.s90@outlook.com>
2017-10-24 22:58:48 -0500

    (cleanup) unnecessary files

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,22 +1,13 @@
-export const INCREMENT_REQUESTED = 'counter/INCREMENT_REQUESTED'
 export const INCREMENT = 'counter/INCREMENT'
-export const DECREMENT_REQUESTED = 'counter/DECREMENT_REQUESTED'
 export const DECREMENT = 'counter/DECREMENT'
 
 const initialState = {
   count: [],
   question: 0,
-  isIncrementing: false,
-  isDecrementing: false
 }
 
 export default (state = initialState, {type, points}) => {
   switch (type) {
-    case INCREMENT_REQUESTED:
-      return {
-        ...state,
-        isIncrementing: true
-      }
 
     case INCREMENT:
       return {
@@ -24,21 +15,13 @@ export default (state = initialState, {type, points}) => {
         previousCount: state.count,
         count: state.count.concat(points),
         question: state.question + 1,
-        isIncrementing: !state.isIncrementing
-      }
-
-    case DECREMENT_REQUESTED:
-      return {
-        ...state,
-        isDecrementing: true
       }
 
     case DECREMENT:
       return {
         ...state,      
         count: state.count.slice(0, -1),
-        question: state.question && state.question - 1,        
-        isDecrementing: !state.isDecrementing
+        question: state.question && state.question - 1,
       }
 
     default:
@@ -49,10 +32,6 @@ export default (state = initialState, {type, points}) => {
 export const increment = (points) => {
   return dispatch => {
     dispatch({
-      type: INCREMENT_REQUESTED,
-    })
-
-    dispatch({
       type: INCREMENT,
       points
     })
@@ -61,10 +40,6 @@ export const increment = (points) => {
 
 export const decrement = (points) => {
   return dispatch => {
-    dispatch({
-      type: DECREMENT_REQUESTED
-    })
-
     dispatch({
       type: DECREMENT,
       points
